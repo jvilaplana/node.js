23de9969ab0188e3a5abfdefdcfa8d93e798198b
Jordi Vilaplana <gatasuna@gmail.com>
2011-09-07 16:42:22 +0200

    Test change.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:1337/');
+ 
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, { "Content-Type": "text/plain" })
+  res.end("Hello world\n");
+});
+ 
+server.listen(80);
